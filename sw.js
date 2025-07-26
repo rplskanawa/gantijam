c5b3dab1c2aff1931cedd751808b36eda450969f
Johan/joese6 <joese6@gmail.com>
2025-07-26 23:16:37 +0700

    projek pwa serah terima jam mengajar

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('form-cache').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png',
+        '/style.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
